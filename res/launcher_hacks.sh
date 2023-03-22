7e903a60e5d6a410552cfcc440f53d360477cb9a
Federico Berti <fberti146+git@gmail.com>
2023-03-22 15:30:09 +0000

    history

--- res/launcher_hacks.sh
+++ res/launcher_hacks.sh
@@ -1,6 +1,6 @@
 #chaotix -> breaks with poll1
-#java -XX:AutoBoxCacheMax=65536 -XX:-DontCompileHugeMethods -XX:+AlwaysCompileLoopMethods -Dhelios.32x.sh2.poll.detect=false -Dtinylog.configuration=./tinylog.properties -Djinput.enable=true -#Djinput.native.location=lib -jar helios32x-23.0316-SNAPSHOT.jar "$@"
+#java -XX:AutoBoxCacheMax=65536 -XX:-DontCompileHugeMethods -XX:+AlwaysCompileLoopMethods -Dkey.config.file=key.config.1p -Dhelios.32x.sh2.poll.detect=false -Dtinylog.configuration=./res/tinylog.properties -Djinput.enable=true -Djinput.native.location=lib -jar helios32x-23.0322.jar "$@"
 
 #fifa96 -> cycles=18 poll0 or cycles 12 poll1
 #brutal -> cycles=12
-#java -XX:AutoBoxCacheMax=65536 -XX:-DontCompileHugeMethods -XX:+AlwaysCompileLoopMethods -Dhelios.32x.sh2.cycles=12 -Dtinylog.configuration=./tinylog.properties -Djinput.enable=true -#Djinput.native.location=lib -jar helios32x-23.0316-SNAPSHOT.jar "$@"
+#java -XX:AutoBoxCacheMax=65536 -XX:-DontCompileHugeMethods -XX:+AlwaysCompileLoopMethods -Dkey.config.file=key.config.1p -Dhelios.32x.sh2.cycles=12 -Dtinylog.configuration=./res/tinylog.properties -Djinput.enable=true -Djinput.native.location=lib -jar helios32x-23.0322.jar "$@"
